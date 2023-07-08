cea0d110570aa24c73af32269fff35e0079c2514
MANOJ <76801009+Manojdhinakar@users.noreply.github.com>
2023-07-08 18:20:40 +0530

    Array using Instantiation

---
+++ ArrayInstantiation.java
@@ -0,0 +1,11 @@
+public class ArrayInstantiation {
+    public static void main(String args[]){
+        int a[]=new int[5];  // Declaration and Instantiation
+        a[0]=100;
+        a[1]=10;
+        a[2]=20;
+        a[3]=30;
+        a[4]=40;
+        System.out.println(a[2]);
+    }
+}
