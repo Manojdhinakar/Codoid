5e683a5cab62f5ecd980bcce7ef76cf3d289d90e
MANOJ <76801009+Manojdhinakar@users.noreply.github.com>
2023-07-08 17:30:01 +0530

    For Loop ,While Loop , DoWhile Loop are executed

---
+++ Loops.java
@@ -0,0 +1,21 @@
+public class Loops {
+    public static void main(String[] args) {
+        /* For Loop */
+        for (int j = 0; j <= 15; j++) {
+            System.out.print(j);
+        }
+        /* While Loop */
+        int i=0;
+        while(i<=10){
+            System.out.println(i);
+            i++;
+        }
+        /* Do While */
+        int k=0;
+        do
+        {
+            System.out.print(k);
+            k++;
+        }while(k<=10);
+    }
+}
