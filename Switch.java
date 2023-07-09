98bde9ec28ab44cd8cf7d5d64fe1a775027387b1
MANOJ <76801009+Manojdhinakar@users.noreply.github.com>
2023-07-09 12:18:11 +0530

    Switch Condition

---
+++ Switch.java
@@ -0,0 +1,13 @@
+class Switch {
+    public static void main(String[] args) {
+        String str = "pass";
+        switch(str){
+            case "pass":
+                System.out.println("pass");
+                break;
+            case "Fail":
+                System.out.println("Fail");
+                break;
+        }
+    }
+}
