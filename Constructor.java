5751b4bba9d3438f7a5b779167b472290a5a8790
MANOJ <76801009+Manojdhinakar@users.noreply.github.com>
2023-07-09 06:41:30 +0530

    Copy values w/o Constructor

--- Constructor.java
+++ Constructor.java
@@ -1,27 +1,20 @@
-class Cart1 {
-    String Product;
-    String Description;
-    int Price;
-    Cart1(String n, String m) {
-        Product = n;
-        Description = m;
+class Student {
+    int id;
+    String name;
+    Student(int i,String n){
+        id=1;
+        name=n;
     }
-    Cart1(String n, String m, int i) {
-        Product = n;
-        Description = m;
-        Price = i;
+    Student(){}
+    void display(){
+        System.out.println(id+" "+name);
     }
-    void display() {
-        System.out.println(Product + "" + Description + "" + Price);
+    public static void main(String args[]){
+        Student s1=new Student(123,"Your Name");
+        Student s2=new Student();
+        s2.id=s1.id;
+        s2.name=s1.name;
+        s1.display();
+        s2.display();
     }
-        public static void main(String args[]){
-            Cart1 s1 = new Cart1("mobile", "with 5g support");
-            Cart1 s2 = new Cart1("Laptop", "With smart WiFi support", 50000);
-            s1.display();
-            s2.display();
-        }
-    }
-
-
-
-
+}
